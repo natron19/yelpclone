5721c9c0a55ba3212bbcf314f2e441ed918c4b32
Nathan R <nathan19@hotmail.com>
2015-04-25 09:46:47 -0700

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader 
   has_many :reviews 
   
+  validates :name, :address, :website, :image, presence: true 
+
+  validates :phone, format: {with: /\A\(\d{3}\) \d{3}-\d{4}\z/, 
+    message: "Must be in format (123) 456-7890"}
+
+  validates :website, format: {with: /\Ahttps?:\/\/.*\z/, 
+    message: "Must start with http://or https://"}
+    
+  validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/, 
+    message: "Address must be in format: 350 Fifth Ave, New York, NY 89334"}
+
+
 end
